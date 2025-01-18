fe1bf6a086fed83a08f723385ec82930e0f1aada
dms901 <dimasmulyadi1245@gmail.com>
2025-01-19 05:24:42 +0700

    update keterangan

--- koneksi2.php
+++ koneksi2.php
@@ -5,8 +5,8 @@
 								password: "",
 								database: "produk_ecommerce"
 	);
-	if(mysqli_connect_errno())
-	{
-		echo "Koneksi Gagal " .mysqli_connect_error() ;
+	if(mysqli_connect_errno())//sebuah void yang mengembalikan pesan kesalahan
+		{
+		echo "Koneksi Gagal " .mysqli_connect_error() ;//sebuah void mengembalikan kode kesalahan (integer)
 	}
 ?>
